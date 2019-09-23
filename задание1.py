b5bc8e3e09779e1fa386e6b4d32d27dd6bb97b05
Zhieniannn <49940771+Zhieniannn@users.noreply.github.com>
2019-09-23 14:49:41 +0300

    Create задание1.py

---
+++ задание1.py
@@ -0,0 +1,11 @@
+a = int(input('a='))
+b = int(input('b='))
+c = int(input('c='))
+d = int(input('d='))
+f = int(input('f='))
+
+df = f - d
+if not df:
+    print('delit nelsa')
+else:
+    print(a * b - c / df)
