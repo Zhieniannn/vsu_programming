c2c6e3c73b08fda0aff94edc74b31cfc1f557b48
Zhieniannn <49940771+Zhieniannn@users.noreply.github.com>
2019-10-07 00:30:07 +0300

    Update Домашка2.номер4.py

--- Домашка2.номер4.py
+++ Домашка2.номер4.py
@@ -1,10 +1,11 @@
 def sk(i):
-  opens = i.count('(')
-closes = i.count(')')
-if opens > closes:
-  return 'нет ', opens - closes, 'закрытой скобки'
-elif opens < closes:
-  return 'нет ', closes - opens, 'открытой скобки'
-return 'скобок нет'
+    opens = i.count('(')
+    closes = i.count(')')
+    if opens > closes:
+        return 'нет ', opens - closes, 'закрытой скобки'
+    elif opens < closes:
+        return 'нет ', closes - opens, 'открытой скобки'
+    return 'скобок нет'
+
 
 print(sk(input('Example: ')))
