4d17913517c35694c0989776a3f02a13449f66a6
Zhieniannn <49940771+Zhieniannn@users.noreply.github.com>
2019-10-07 00:34:39 +0300

    Update Домашка2.номер4.py

--- Домашка2.номер4.py
+++ Домашка2.номер4.py
@@ -2,9 +2,9 @@ def sk(i):
     opens = i.count('(')
     closes = i.count(')')
     if opens > closes:
-        return 'нет ', opens - closes, 'закрытой скобки'
+        return 'нет ', opens - closes, ' закрытой скобки'
     elif opens < closes:
-        return 'нет ', closes - opens, 'открытой скобки'
+        return 'нет ', closes - opens, ' открытой скобки'
     return 'скобок нет'
 
 
